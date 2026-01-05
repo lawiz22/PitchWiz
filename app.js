dfb8811758ef564515a97b61c6d2b21fd1bd5c84
Lawiz <lawiz2222@gmail.com>
2026-01-05 13:55:51 -0500

    Reverse vertical pan direction: drag up pans up, drag down pans down

--- app.js
+++ app.js
@@ -686,7 +686,7 @@ function init() {
 
             // Vertical drag = vertical pan (move up/down to see different notes)
             const currentPan = visualizer.verticalPan || 0;
-            const panChange = -deltaY * 0.05; // Negative because drag down should pan down
+            const panChange = deltaY * 0.05; // Drag up = pan up, drag down = pan down
             visualizer.verticalPan = currentPan + panChange;
         }
     }, { passive: false });
